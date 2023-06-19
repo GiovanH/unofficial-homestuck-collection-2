c8b308420529d5e8f131f735f7b5c0acf52d93ac
Gio <sethg@ipi.org>
2023-06-19 18:31:51 -0500

    WIP

--- src/components/Page/PageMetadata.vue
+++ src/components/Page/PageMetadata.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="metadata">
-    <div :title="$getChapter(thisPage.pageId)">
-      <p v-text="`${thisPage.storyId}/${thisPage.pageId}`"/>
-      <p v-html="vizLink"/>
+  <div class="container">
+    <div class="metadata">
+      <div :title="$getChapter(thisPage.pageId)">
+        <p v-text="pageId"/>
+        <p v-html="vizLink"/>
+      </div>
+      <time
+        :datetime="datetime.toFormat('yyyy-MM-dd ttt')"
+        :data-timestamp="thisPage.timestamp">
+        <span v-text="datetime.toFormat('LLL dd yyyy')"/>
+        <span v-text="datetime.toFormat('t ZZZZ')"/>
+      </time>
     </div>
-    <time
-      :datetime="datetime.toFormat('yyyy-MM-dd ttt')"
-      :data-timestamp="thisPage.timestamp">
-      <span v-text="datetime.toFormat('LLL dd yyyy')"/>
-      <span v-text="datetime.toFormat('t ZZZZ')"/>
-    </time>
+    <div class="metadata tagbox">
+      <table>
+        <tr><th></th><th>Tag</th><th>#</th></tr>
+        <tr v-for="count, tag in tags">
+          <td class="link" v-if="ourtags.includes(tag)">-</td>
+          <td class="link" v-else @click="doVoteFor(tag)">+</td>
+          <td v-text="tag" />
+          <td v-text="count" />
+        </tr>
+      </table>
+      <input @keydown.enter="submitNewTag"></input>
+    </div>
   </div>
 </template>
 
@@ -23,10 +37,54 @@ export default {
   },
   data: function() {
     return {
-      DateTime: require('luxon').DateTime
+      DateTime: require('luxon').DateTime,
+      taghost: "http://127.0.0.1:5000",
+      tags: {},
+      ourtags: [],
     }
   },
+  methods: {
+    updateTags() {
+      this.tags = {}
+      this.ourtags = []
+
+      fetch(`${this.taghost}/${this.pageIdSlug}/tags`)
+        .then(r => r.json())
+        .then(json => this.tags = json)
+
+      fetch(`${this.taghost}/${this.pageIdSlug}/own`)
+        .then(r => r.json())
+        .then(json => this.ourtags = json)
+    },
+    submitNewTag(event) {
+      const value = event.target.value
+      event.target.value = ""
+      this.doVoteFor(value)
+    },
+    doVoteFor(value) {
+      this.$logger.info("Voting", value)
+
+      fetch(`${this.taghost}/${this.pageIdSlug}/vote/${value}`)
+        .then(r => {
+          this.updateTags()
+        })
+    }
+  },
+  watch: {
+    'thisPage'(to, from) {
+      this.updateTags()
+    },
+  },
+  mounted() {
+    this.updateTags()
+  },
   computed: {
+    pageId() {
+      return `${this.thisPage.storyId}/${this.thisPage.pageId}`
+    },
+    pageIdSlug() {
+      return this.pageId.replace('/', '-')
+    },
     datetime() {
       if (!this.thisPage.timestamp) {
         return undefined
@@ -42,17 +100,24 @@ export default {
       const viz = this.$mspaToViz(this.thisPage.pageId)
       return `${viz.s}/&ZeroWidthSpace;${viz.p}` 
     }
-  },
-  methods: {
   }
 }
 </script>
 
 <style scoped lang="scss">
+.container {
+  position: absolute;
+  left: 7px;
+
+  input {
+    max-width: 100%;
+    width: calc(100% - 10px);
+  }
+}
+
+
 .pageBody {
   .metadata {
-    position: absolute;
-    left: 7px;
     font-size: 117%;
     max-width: 135px;
     background-color: rgba(255, 255, 255, 0.5);
@@ -60,12 +125,27 @@ export default {
     border-top-color: white;
     border-left-color: white;
   }
+  .tagbox {
+    table {
+      width: 100%;
+    }
+    td {
+      font-size: 14px;
+      font-family: sans-serif;
+    }
+    .link {
+      // text-decoration: underline;
+      // &:hover {
+        cursor: pointer;
+      // }
+    }
+  }
   // WIP: Nudge box out of supercartridge panels
   // &.supercartridge .metadata {
   //   left: calc(-135px - 7px);
   // }
 }
-.x2 .pageBody.two .metadata { 
+.x2 .pageBody.two .container {
   left: 630px; width: 135px; 
 }
 
